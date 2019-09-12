2a1640197bc69e76a844b5f12aae05a1f73b6ba1
Cafelatte <tkdgus3078@gmail.com>
2019-09-12 21:34:38 +0900

    vuetify add custom theme

--- src/App.vue
+++ src/App.vue
@@ -21,22 +21,17 @@
   </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import HelloWorld from './components/HelloWorld';
+import './App.scss'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
